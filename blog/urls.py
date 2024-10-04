cbaaf6f770ec17194699217cf7ef5a4fac6fa805
kmin1231 <kmin91143@gmail.com>
2024-10-05 08:17:11 +0900

    img_blog project merged

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+router = routers.DefaultRouter()
+router.register('Post', views.BlogImage)
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('api_root/', include(router.urls)),
+    path('/post_list', views.post_list, name='post_list'),
+    # path('', views.poem, name='poem'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
